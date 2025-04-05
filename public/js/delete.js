35e008d59598c1ec4c5a257ca78704295dd27759
AmaneYodo <amane.yodo@gmail.com>
2025-04-05 18:54:59 +0900

    コメントを削除

--- public/js/delete.js
+++ public/js/delete.js
@@ -6,14 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
             let confirmMessage = '';
 
-            // タスク削除とリスト削除でメッセージを分ける
             if (itemType === 'task') {
                 confirmMessage = "本当にこのタスクを削除しますか？";
             } else if (itemType === 'list') {
                 confirmMessage = "本当にこのリストを削除しますか？";
             }
 
-            // メッセージが設定されていれば確認ダイアログを表示
             if (confirm(confirmMessage)) {
                 form.submit();
             }
